c0ce51b215ece49644a688effba2366275c1e2a9
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-02-29 20:48:52 -0300

    TS-1: Criando o projeto

---
+++ src/main/java/br/com/timesync/TimeSyncApplication.java
@@ -0,0 +1,13 @@
+package br.com.timesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/timesync/TimeSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.timesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
