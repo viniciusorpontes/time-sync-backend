1e1c418aa180d383daa6d1c7fc7b99437e33d3a7
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-03-10 01:59:16 -0300

    TS-2: Adicionando novo campo "ativado" para permitir a exclusão lógica de serviços

--- src/main/resources/db/migration/V001__criando_tabela_de_servicos.sql
+++ src/main/resources/db/migration/V001__criando_tabela_de_servicos.sql
@@ -3,5 +3,6 @@ CREATE TABLE servico
     id    SERIAL        NOT NULL PRIMARY KEY,
     nome  VARCHAR(100)  NOT NULL,
     tempo TIME          NOT NULL,
-    valor DECIMAL(8, 2) NOT NULL
+    valor DECIMAL(8, 2) NOT NULL,
+    ativo BOOLEAN       NOT NULL
 );
