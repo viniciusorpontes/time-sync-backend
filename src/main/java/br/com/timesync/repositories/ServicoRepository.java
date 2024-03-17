0c33ede74ec488c70fb23183d8a3ea9e1a0d3736
Matheus Marchioni <matheus.marchioni@icloud.com>
2024-03-17 17:33:05 -0300

    TS-5:Adicionado Object para busca de usuarioID

--- src/main/java/br/com/timesync/repositories/ServicoRepository.java
+++ src/main/java/br/com/timesync/repositories/ServicoRepository.java
@@ -4,7 +4,11 @@ import br.com.timesync.entities.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+
+    Optional<Object> findByUsuarioId(Integer usuarioId);
 
 }
