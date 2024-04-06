b04fb731ed14297e7e4b38a1d4b0f14c998b75e2
Matheus Marchioni <matheus.marchioni@icloud.com>
2024-04-05 21:34:47 -0300

    TS-5: Alterado nome da constraint

--- src/main/resources/db/migration/V005__add_coluna_usuario_na_tabela_de_servico.sql
+++ src/main/resources/db/migration/V005__add_coluna_usuario_na_tabela_de_servico.sql
@@ -2,7 +2,7 @@ ALTER TABLE servico
     ADD usuario_id INTEGER;
 
 ALTER TABLE servico
-    ADD CONSTRAINT fk_constraint_name
+    ADD CONSTRAINT fk_servico_usuario
         FOREIGN KEY (usuario_id) REFERENCES usuario(id);
 
 
