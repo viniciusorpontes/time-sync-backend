d31466bfb3b416d852753603493832abba8f9fd6
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-05-14 20:04:57 -0300

    TS-7: Implementando camada de segurança na aplicação com Spring Security

---
+++ src/main/java/br/com/timesync/security/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package br.com.timesync.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+}
