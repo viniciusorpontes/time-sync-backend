77aa6085e94fad99a1e3095a2ed075891407fb91
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-06-19 23:47:45 -0300

    TS-7: Correção AgendamentoDTO.java

--- src/main/java/br/com/timesync/dto/AgendamentoDTO.java
+++ src/main/java/br/com/timesync/dto/AgendamentoDTO.java
@@ -3,6 +3,6 @@ package br.com.timesync.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record AgendamentoDTO(LocalDateTime dataChegad, List<Integer> idsServicos, Integer clienteId, Integer consumidorId) {
+public record AgendamentoDTO(LocalDateTime dataChegada, List<Integer> idsServicos, Integer clienteId, Integer consumidorId) {
 
 }
