d996527d7cd6e795004423e052014b59bf35e2dd
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-06-20 00:16:44 -0300

    TS-7: Passando responsabilidade de gerar token para o Controller LoginController

---
+++ src/main/java/br/com/timesync/controllers/LoginController.java
@@ -0,0 +1,36 @@
+package br.com.timesync.controllers;
+
+import br.com.timesync.dto.LoginRequestDTO;
+import br.com.timesync.dto.LoginResponseDTO;
+import br.com.timesync.entities.Usuario;
+import br.com.timesync.services.LoginService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@AllArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        //Transforma o LoginRequestDTO em um objeto UsernamePasswordAuthenticationToken próprio do Spring Security
+        final var authenticationToken = new UsernamePasswordAuthenticationToken(loginRequestDTO.email(), loginRequestDTO.senha());
+        // Autentica o objeto UsernamePasswordAuthenticationToken e retorna um Usuario
+        final var usuario = (Usuario) authenticationManager.authenticate(authenticationToken).getPrincipal();
+        // Gera um token para o Usuario logado
+        final var tokenJWT = loginService.gerarToken(usuario);
+        // Insere o Token JWT no objeto LoginResponseDTO
+        return ResponseEntity.ok(new LoginResponseDTO(tokenJWT));
+    }
+
+}
--- src/main/java/br/com/timesync/controllers/UsuarioController.java
+++ src/main/java/br/com/timesync/controllers/UsuarioController.java
@@ -1,12 +1,9 @@
 package br.com.timesync.controllers;
 
-import br.com.timesync.dto.LoginDTO;
 import br.com.timesync.entities.Usuario;
 import br.com.timesync.services.UsuarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -18,7 +15,6 @@ import java.net.URI;
 public class UsuarioController {
 
     private final UsuarioService usuarioSevice;
-    private final AuthenticationManager authenticationManager;
 
     @GetMapping("/buscarPorId")
     public ResponseEntity<Usuario> buscarPorId(@RequestParam Integer id) {
@@ -30,13 +26,6 @@ public class UsuarioController {
         usuario = this.usuarioSevice.salvar(usuario);
         final URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId()).toUri();
         return ResponseEntity.created(uri).body(usuario);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginDTO login) {
-        var token = new UsernamePasswordAuthenticationToken(login.email(), login.senha());
-        var authentication = authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
     }
 
 }
