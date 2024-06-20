7c4b601c447db78009b4d77b1faf05b4c957a555
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-06-20 00:43:09 -0300

    TS-7: Criando usuário ADMINISTRADOR, CLIENTE e CONSUMIDOR

---
+++ src/main/resources/db/migration/V006__add_usuario_administrador.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuarios (cpf, nome, email, telefone, senha, tipo, ativo)
+VALUES ('460.665.978-50', 'Vinicius Pontes', 'viniciuspontes258@gmail.com', '14998371897',
+        '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.', 0, true);
