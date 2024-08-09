0a84692104c113b0ef7ee60a5e043386dc99cbd6
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-08-09 20:43:04 -0300

    feat(TS-6) - Refatorando método findByUsuarioId

--- src/main/java/br/com/timesync/repositories/ServicoRepository.java
+++ src/main/java/br/com/timesync/repositories/ServicoRepository.java
@@ -2,6 +2,8 @@ package br.com.timesync.repositories;
 
 import br.com.timesync.entities.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,10 @@ import java.util.List;
 @Repository
 public interface ServicoRepository extends JpaRepository<Servico, Integer> {
 
-    List<Servico> findByUsuarioId(Integer usuarioId);
+    @Query(value = "SELECT * FROM servicos s " +
+            "INNER JOIN servicos_usuarios su ON s.id = su.servico_id " +
+            "WHERE su.usuario_id = :usuario_id",
+            nativeQuery = true)
+    List<Servico> findByUsuarioId(@Param(value = "usuario_id") Integer usuario_id);
 
 }
