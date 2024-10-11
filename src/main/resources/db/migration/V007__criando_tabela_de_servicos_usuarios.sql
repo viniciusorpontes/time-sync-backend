b91618d6e484fb86b5246fdba6f1c143d0ed224e
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-10-11 19:56:14 -0300

    feat(TS-8) - Correção no script

--- src/main/resources/db/migration/V007__criando_tabela_de_servicos_usuarios.sql
+++ src/main/resources/db/migration/V007__criando_tabela_de_servicos_usuarios.sql
@@ -1,6 +1,6 @@
 CREATE TABLE servicos_usuarios
 (
-    servico_id INTEGER REFERENCES agendamentos (id),
-    usuario_id INTEGER REFERENCES servicos (id),
+    servico_id INTEGER REFERENCES servicos (id),
+    usuario_id INTEGER REFERENCES usuarios (id),
     PRIMARY KEY (servico_id, usuario_id)
 );
